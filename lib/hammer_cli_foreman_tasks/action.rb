a3aaa42dc5902db0e0a55d0cc96685832e882e56
Adam Ruzicka <aruzicka@redhat.com>
2014-05-22 11:58:28 +0200

    Revamped, lots of cool stuff

--- lib/hammer_cli_foreman_tasks/action.rb
+++ lib/hammer_cli_foreman_tasks/action.rb
@@ -7,9 +7,12 @@ module HammerCLIForemanTasks
 
       command_name 'export'
 
-      option '--task-id', 'task_id', "ID of task to export"
-      option '--plan-id', 'plan_id', "ID of plan to export"
-      option '--action-id', 'action_id', "ID of action to export", :required => true
+      option ['-t', '--task-id'], 'task_id', "ID of task to export"
+      option ['-e', '--exec-plan-id'], 'plan_id', "ID of plan to export"
+      option '--action-id', 'action_id', "ID of action to export",
+             :required => true,
+             :format => HammerCLI::Options::Normalizers::List.new
+      option ["-d", "--dir"], "DIR", "Output to DIR"
 
       validate_options do
         any(:option_task_id, :option_plan_id).required
@@ -17,10 +20,35 @@ module HammerCLIForemanTasks
       end
 
       def execute
-        id = @option_plan_id.nil? ? task_to_plan_id(@option_task_id) : @option_plan_id
-        print_message(DynflowBinding.new.get_action(id, @option_action_id))
+        plan_id = @option_plan_id.nil? ? task_to_plan_id(@option_task_id) : @option_plan_id
+        path = @option_dir.nil? ? '.' : @option_dir
+        path.gsub!(/\/$/,'')
+        FileUtils.mkdir_p("#{path}/#{plan_id}") unless File.exists?("#{path}/#{plan_id}")
+        export_actions(plan_id, @option_action_id, path)
         HammerCLI::EX_OK
       end
+
+      def export_actions(plan_id, action_ids, path)
+        Dir.mktmpdir do |tmp|
+          Dir.chdir(tmp)
+          FileUtils.mkdir(plan_id) unless Dir.exists?(plan_id)
+          action_ids.each do |action_id|
+            dump_action(plan_id, action_id)
+          end
+          FileUtils.cp_r(plan_id, path)
+        end
+      end
+
+      def dump_action(plan_id, action_id)
+        begin
+          File.write("#{plan_id}/action-#{action_id}.json",
+                     get_action(plan_id, action_id))
+        rescue Exception => e
+          File.exists?("#{plan_id}/action-#{action_id}.json") && File.delete("#{plan_id}/action-#{action_id}.json")
+          # TODO ERROR LOG
+        end
+      end
+
     end
 
     autoload_subcommands
--- lib/hammer_cli_foreman_tasks/task.rb
+++ lib/hammer_cli_foreman_tasks/task.rb
@@ -25,19 +25,74 @@ module HammerCLIForemanTasks
       include HammerCLIForemanTasks::Helper
 
       command_name 'export'
-      option "--task-id", "TASK_ID", "ID of task to export"
-      option "--plan-id", "PLAN_ID", "ID of plan to export"
+      option ["-t", "--task-id"], "TASK_ID", "ID of task to export", :format => HammerCLI::Options::Normalizers::List.new
+      option ["-e", "--exec-plan-id"], "PLAN_ID", "ID of plan to export",  :format => HammerCLI::Options::Normalizers::List.new
+      option ["-p", "--paused"], :flag, "Operate on all paused tasks"
+      option ["-c", "--compression"], :flag, "Use gzip compression"
+      option ["-d", "--dir"], "DIR", "Output to DIR"
+      option ["-a", "--all"], :flag, "Operate on all tasks"
+      option ["-f", "--full"], :flag, "Export task WITH all actions"
 
       validate_options do
-        any(:option_task_id, :option_plan_id).required
-        all(:option_task_id, :option_plan_id).exist? && all(:option_task_id, :option_plan_id).rejected
+        any(:option_task_id, :option_plan_id, :option_paused, :option_all).required
       end
 
       def execute
-        id = @option_plan_id.nil? ? task_to_plan_id(@option_task_id) : @option_plan_id
-        print_message(DynflowBinding.new.get_execution_plan(id))
+        plan_ids = get_all_ids unless @option_all.nil?
+        plan_ids = get_all_ids(true) unless @option_paused.nil?
+        plan_ids ||= []
+        plan_ids << @option_plan_id
+        plan_ids << @option_task_id.map { |task_id| task_to_plan_id(task_id) }
+        plan_ids.flatten.uniq.compact
+
+        plan_ids.each do |plan_id|
+          export_plan(plan_id, @option_dir, ! @option_full.nil?)
+        end
         HammerCLI::EX_OK
       end
+
+      def get_all_ids(only_paused = false)
+        MultiJson.load(only_paused ? get_paused : get_all)
+      end
+
+      def export_plan(plan_id, path, with_action = false)
+        Dir.mktmpdir do |tmp|
+          Dir.chdir(tmp)
+          begin
+            dump_plan(plan_id)
+          rescue Exception => e
+            # TODO ERROR LOG
+            return
+          end
+          dump_plan_actions(plan_js) if with_action
+          if @option_compression.nil?
+            FileUtils.cp_r(plan_id, path)
+          else
+            compress(plan_id)
+            FileUtils.cp("#{plan_id}.tar.gz", path)
+          end
+        end
+      end
+
+      def dump_plan(plan_id, path = '.')
+        dest = "#{path}/#{plan_id}"
+        FileUtils.mkdir_p(dest) unless File.exist?(dest)
+        plan_js = get_execution_plan(plan_id)
+        File.write("#{dest}/plan.json", plan_js)
+      end
+
+      def compress(target)
+        archive = "#{target}.tar.gz"
+        tgz = Zlib::GzipWriter.new(File.open(archive, 'wb'))
+        Archive::Tar::Minitar.pack(target, tgz)
+      end
+
+      def dump_plan_actions(plan_js)
+        plan = MultiJson.load(plan_js, :symbolize_keys => true)
+        action_ids = plan['steps'].map { |step| step['action_id'] }
+        action_ids.each { |action_id| dump_action(plan_id, action_id) }
+      end
+
     end
 	
 		self.subcommand 'action', "Manipulate task's actions", HammerCLIForemanTasks::Action
