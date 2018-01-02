58ab86c20e8c9227b5e8a2aaecd5a5e03cfbcbf8
Amit Karsale <karsale.amit@gmail.com>
2018-01-02 16:25:47 +0530

    Fixes #17358 - task progress accepts NIL value for id (#31)

--- lib/hammer_cli_foreman_tasks/helper.rb
+++ lib/hammer_cli_foreman_tasks/helper.rb
@@ -1,17 +1,19 @@
 module HammerCLIForemanTasks
   module Helper
-
     # render the progress of the task using polling to the task API
     def task_progress(task_or_id)
       task_id = task_or_id.is_a?(Hash) ? task_or_id['id'] : task_or_id
-      task_progress = TaskProgress.new(task_id) { |id| load_task(id) }
-      task_progress.render
-      task_progress.success?
+      if !task_id.empty?
+        task_progress = TaskProgress.new(task_id) { |id| load_task(id) }
+        task_progress.render
+        task_progress.success?
+      else
+        signal_usage_error(_('Please mention appropriate attribute value'))
+      end
     end
 
     def load_task(id)
       HammerCLIForeman.foreman_resource!(:foreman_tasks).call(:show, :id => id)
     end
-
   end
 end
