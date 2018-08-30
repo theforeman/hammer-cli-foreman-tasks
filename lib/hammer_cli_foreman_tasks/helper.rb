68b71a523991feae7246dbdab1289b09526a7600
Oleh Fedorenko <ofedoren@redhat.com>
2018-08-30 12:37:07 +0000

    Fixes #24581 - Option to completely suspend progress bar

--- lib/hammer_cli_foreman_tasks/helper.rb
+++ lib/hammer_cli_foreman_tasks/helper.rb
@@ -4,7 +4,8 @@ module HammerCLIForemanTasks
     def task_progress(task_or_id)
       task_id = task_or_id.is_a?(Hash) ? task_or_id['id'] : task_or_id
       if !task_id.empty?
-        task_progress = TaskProgress.new(task_id) { |id| load_task(id) }
+        options = { verbosity: @context[:verbosity] || HammerCLI::V_VERBOSE }
+        task_progress = TaskProgress.new(task_id, options) { |id| load_task(id) }
         task_progress.render
         task_progress.success?
       else
