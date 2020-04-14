aff0538b0501286115ac4910118c3a95c608dade
MariaAga <mariaagaphonchev@gmail.com>
2020-04-14 14:38:07 +0300

    Fixes #28940 - add hammer command to clear cancelled logics (#40)

--- lib/hammer_cli_foreman_tasks/recurring_logic.rb
+++ lib/hammer_cli_foreman_tasks/recurring_logic.rb
@@ -26,6 +26,38 @@ module HammerCLIForemanTasks
       failure_message _('Could not cancel the recurring logic')
       build_options
     end
+    
+    class DeleteCommand < HammerCLIForeman::DeleteCommand
+      action :bulk_destroy
+
+      option '--cancelled', :flag, _("Only delete cancelled recurring logics")
+      option '--finished', :flag, _("Only delete finished recurring logics")
+      def request_params
+        params = super
+        raise ArgumentError, "Please specify if you want to remove cancelled or finished recurring logics using --cancelled or --finished." unless (options['option_cancelled'] || options["option_finished"])
+        raise ArgumentError, "Please only use one of the arguments at a time." if (options['option_cancelled'] && options["option_finished"])
+        cancelled = "state=cancelled" if options["option_cancelled"]
+        finished = "state=finished" if options["option_finished"]
+        params["search"] = cancelled || finished
+        params
+      end
+
+      command_name 'delete'
+      desc _("Delete all recuring logics filtered by the arguments")
+      failure_message _('Could not delete recurring logics')
+      output ListCommand.output_definition
+
+      def execute
+        response = send_request
+        if response.length > 0
+          puts _('The following recurring logics deleted:') + "\n"
+          print_data(response)
+        else
+          puts _("No recurring logics deleted.")
+        end
+        HammerCLI::EX_OK
+      end
+    end
 
     autoload_subcommands
   end
