292ab0109b21e02a78941d807ac377e47bd67a66
Chris Roberts <chrobert@redhat.com>
2020-08-28 12:43:44 -0400

    Fixes #30733 - Add logic for returned job_invocation json

--- lib/hammer_cli_foreman_tasks/helper.rb
+++ lib/hammer_cli_foreman_tasks/helper.rb
@@ -2,7 +2,14 @@ module HammerCLIForemanTasks
   module Helper
     # render the progress of the task using polling to the task API
     def task_progress(task_or_id)
-      task_id = task_or_id.is_a?(Hash) ? task_or_id['id'] : task_or_id
+      task_id = case task_or_id
+      when Hash
+        task_or_id['id']
+      when Array
+        Hash[*task_or_id.flatten(1)]['task_id']
+      else
+        task_or_id
+      end
       if !task_id.empty?
         options = { verbosity: @context[:verbosity] || HammerCLI::V_VERBOSE }
         task_progress = TaskProgress.new(task_id, options) { |id| load_task(id) }
